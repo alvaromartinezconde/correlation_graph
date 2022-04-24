aeb602b29feed61ffb5bf802127dc45eb3f8694f
Alvaro Martinez-Conde <alvaromartinezconde@ucm.es>
2022-04-24 20:38:22 +0200

    Created repo for correlation graph

--- grafico_correlacion.r
+++ grafico_correlacion.r
@@ -2,6 +2,11 @@ library(GGally)
 library(corrplot)
 library(dplyr)
 
+# Lectura de archivos
+
+library("readxl")
+futbol1 <- read_excel("C:/Users/mcond/OneDrive/Escritorio/Liga Santander.xlsx")
+
 corrplot(cor(select(futbol1, goles_local, primera_visitante, remates_local, remates_visitante, 
                       faltas_local, faltas_visitante ,corners_local, 
                       amarillas_local, amarillas_visitante)),
